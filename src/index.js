d63b203893713c0ef1d10a295a5a0b56724cca14
Elonatech Nigeria Limited <support@elonatech.com.ng>
2024-11-12 17:37:09 +0100

    app-page

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './components/admin/AuthContext';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<AuthProvider>
-    <HelmetProvider>
-        <App />
-    </HelmetProvider>
-</AuthProvider>
+    <AuthProvider>
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
+    </AuthProvider>
 );
 
 
